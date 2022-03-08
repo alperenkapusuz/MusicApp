82323a5bde42a674cb6921900985fdb2f8a2f05e
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2022-03-08 19:35:12 +0300

    newsApp tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import {SafeAreaView, Text, View, StyleSheet, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, StyleSheet, FlatList} from 'react-native';
 import music_data from './music-data.json';
 import SongCard from './components/SongCard';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [list, setList] = useState(music_data);
   const renderSong = ({item}) => <SongCard song={item} />;
-  const renderSeperator = () => <View style={styles.seperator}/>
+  const renderSeperator = () => <View style={styles.seperator} />;
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+    setList(filteredList);
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <FlatList
-          keyExtractor={item => item.id}
-          data={music_data}
-          renderItem={renderSong}
-          ItemSeparatorComponent={renderSeperator}
-        />
-      </View>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={item => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeperator}
+      />
     </SafeAreaView>
   );
 }
